ad30305056fd3a124310c481b238b06f8987b378
Angel Figuera <angeleraser@gmail.com>
2021-09-09 23:11:59 -0400

    feat: add metaweather api interfaces

---
+++ src/core/domain/models/metaweather.api.ts
@@ -0,0 +1,41 @@
+interface MetaweatherLocalization {
+	latt_long: string;
+	location_type: string;
+	title: string;
+	woeid: number;
+}
+
+interface MetaweatherOnEarthLocalization {
+	consolidated_weather: Array<MetaweatherConsolidatedWeather>;
+	latt_long: string;
+	location_type: string;
+	parent: MetaweatherLocalization;
+	sources: Array<unknown>;
+	sun_rise: string;
+	sun_set: string;
+	time: string;
+	timezone_name: string;
+	timezone: string;
+	title: string;
+	woeid: number;
+}
+
+interface MetaweatherConsolidatedWeather {
+	id: number;
+	weather_state_name: string;
+	weather_state_abbr: string;
+	wind_direction_compass: string;
+	created: string;
+	applicable_date: string;
+	min_temp: number;
+	max_temp: number;
+	the_temp: number;
+	wind_speed: number;
+	wind_direction: number;
+	air_pressure: number;
+	humidity: number;
+	visibility: number;
+	predictability: number;
+}
+
+export { MetaweatherLocalization, MetaweatherOnEarthLocalization };
