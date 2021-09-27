b0f49984d42aa02068a1b09aea18f2300da4d3f9
Angel Figuera <angeleraser@gmail.com>
2021-09-26 23:31:53 -0400

    feat: create weather mini card component

--- src/components/weather-content.vue
+++ src/components/weather-content.vue
@@ -4,15 +4,24 @@
 			<v-btn color="white" round> °C </v-btn>
 			<v-btn color="pale-blue" round> °F </v-btn>
 		</div>
+
+		<div class="weather-content__weather-cards">
+			<weather-mini-card />
+			<weather-mini-card />
+			<weather-mini-card />
+			<weather-mini-card />
+			<weather-mini-card />
+		</div>
 	</div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import VBtn from './v-btn.vue';
+import WeatherMiniCard from './weather-mini-card.vue';
 
 export default Vue.extend({
-	components: { VBtn },
+	components: { VBtn, WeatherMiniCard },
 });
 </script>
 
@@ -23,11 +32,17 @@ export default Vue.extend({
 	padding: 24px 16px;
 	height: 100%;
 	width: 100%;
+	max-width: 1140px;
+	margin: 0 auto;
+
+	& > *:not(:last-child) {
+		margin-bottom: 68px;
+	}
 
 	&__call-to-actions {
 		width: 100%;
 		display: flex;
-		justify-content: flex-end;
+		justify-content: center;
 
 		.v-btn {
 			font-weight: 700;
@@ -38,8 +53,31 @@ export default Vue.extend({
 		}
 	}
 
+	&__weather-cards {
+		display: grid;
+		grid-template-columns: repeat(auto-fill, 120px);
+		justify-content: center;
+		justify-items: center;
+		gap: 26px;
+		width: 100%;
+		max-width: calc(120px * 6);
+		margin: 0 auto;
+	}
+
 	@media screen and (min-width: $breakpoint) {
 		padding: 40px 48px;
+	}
+
+	@media screen and (min-width: 1280px) {
+		&__call-to-actions {
+			justify-content: flex-end;
+		}
+
+		&__weather-cards {
+			grid-template-columns: repeat(5, 120px);
+			justify-content: space-between;
+			max-width: 100%;
+		}
 	}
 }
 </style>
---
+++ src/components/weather-mini-card.vue
@@ -0,0 +1,65 @@
+<template>
+	<div class="weather-mini-card">
+		<div class="weather-mini-card__content">
+			<div class="weather-mini-card__content__date">Sun, 7 Jun</div>
+
+			<div class="weather-mini-card__content__img">
+				<weather-state-img
+					width="56"
+					height="62"
+					src="https://www.metaweather.com/static/img/weather/hr.svg"
+					alt="Weather icon"
+					aria-label="Weather state representative image"
+				/>
+			</div>
+
+			<div class="weather-mini-card__content__temperature">
+				<div class="weather-mini-card__content__temperature__current">16 C</div>
+				<div class="weather-mini-card__content__temperature__min">11 C</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import WeatherStateImg from './weather-state-img.vue';
+
+export default Vue.extend({
+	components: { WeatherStateImg },
+});
+</script>
+
+<style lang="scss">
+.weather-mini-card {
+	padding: 18px 22px;
+	background-color: $dark-blue;
+
+	&__content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		font-size: 16px;
+
+		&__img {
+			margin: 12px 0 26px;
+		}
+
+		&__temperature {
+			display: flex;
+			width: 100%;
+			justify-content: space-between;
+			font-weight: $text-semibold;
+
+			&__current {
+				color: $white;
+				margin-right: 12px;
+			}
+
+			&__min {
+				color: $gray-2;
+			}
+		}
+	}
+}
+</style>
